cd93e9c52b954ae826db7592ca09687ec6782745
Bram de Jong <805269+bdejong@users.noreply.github.com>
2025-02-17 16:25:55 +0100

    Cleaned up submit_log.cpp + removed memory leak

--- src/api-client/submit_log.cpp
+++ src/api-client/submit_log.cpp
@@ -3,81 +3,61 @@
 #include "trmnl_log.h"
 #include <WiFiClientSecure.h>
 #include <HTTPClient.h>
+#include <memory>
 
 bool submitLogToApi(LogApiInput &input, const char *api_url)
 {
   String payload = "{\"log\":{\"logs_array\":[" + String(input.log_buffer) + "]}}";
 
-  WiFiClientSecure *secureClient = new WiFiClientSecure;
-  WiFiClient *insecureClient = new WiFiClient;
+  bool isHttps = String(api_url).indexOf("https://") == 0;
 
-  secureClient->setInsecure();
-
-  bool isHttps = true;
-  if (String(api_url).indexOf("https://") == -1)
-  {
-    isHttps = false;
-  }
-
-  // define client depending on the isHttps variable
-  WiFiClient *client = isHttps ? secureClient : insecureClient;
-
-  bool result = false;
-  if (client)
-  {
-    {
-      // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
-      HTTPClient https;
-      Log_info("[HTTPS] begin /api/log ...");
-
-      char new_url[200];
-      strcpy(new_url, api_url);
-      strcat(new_url, "/api/log");
-
-      if (https.begin(*client, new_url))
-      { // HTTPS
-        Log_info("[HTTPS] POST...");
-
-        https.addHeader("Accept", "application/json");
-        https.addHeader("Access-Token", input.api_key);
-        https.addHeader("Content-Type", "application/json");
-        Log_info("Send log - %s", payload.c_str());
-        // start connection and send HTTP header
-        int httpCode = https.POST(payload);
-
-        // httpCode will be negative on error
-        if (httpCode > 0)
-        {
-          // HTTP header has been send and Server response header has been handled
-          Log_info("[HTTPS] POST... code: %d", httpCode);
-          // file found at server
-          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY || httpCode == HTTP_CODE_NO_CONTENT)
-          {
-            Log_info("[HTTPS] POST OK");
-            result = true;
-          }
-        }
-        else
-        {
-          Log_error("[HTTPS] POST... failed, error: %d %s", httpCode, https.errorToString(httpCode).c_str());
-          result = false;
-        }
-
-        https.end();
-      }
-      else
-      {
-        Log_error("[HTTPS] Unable to connect");
-        result = false;
-      }
-    }
-
-    delete client;
-  }
+  std::unique_ptr<WiFiClient> client;
+  
+  // make_unique might throw bad_alloc if out of memory
+  if (isHttps)
+    client = std::make_unique<WiFiClientSecure>();
   else
+    client = std::make_unique<WiFiClient>();
+  
+  HTTPClient https;
+  Log_info("[HTTPS] begin /api/log ...");
+
+  char new_url[200];
+  strcpy(new_url, api_url);
+  strcat(new_url, "/api/log");
+
+  if (!https.begin(*client, new_url))
   {
-    Log_error("[HTTPS] Unable to create client");
-    result = false;
+    Log_error("[HTTPS] Unable to connect");
+    return false;
   }
-  return result;
+
+  Log_info("[HTTPS] POST...");
+
+  https.addHeader("Accept", "application/json");
+  https.addHeader("Access-Token", input.api_key);
+  https.addHeader("Content-Type", "application/json");
+  Log_info("Send log - %s", payload.c_str());
+  
+  // start connection and send HTTP header
+  int httpCode = https.POST(payload);
+
+  // httpCode will be negative on error
+  if (httpCode < 0)
+  {
+    Log_error("[HTTPS] POST... failed, error: %d %s", httpCode, https.errorToString(httpCode).c_str());
+    return false;
+  }
+  else if (httpCode != HTTP_CODE_OK && httpCode != HTTP_CODE_MOVED_PERMANENTLY && httpCode != HTTP_CODE_NO_CONTENT)
+  {
+    Log_error("[HTTPS] POST... failed, returned HTTP code unknown: %d %s", httpCode, https.errorToString(httpCode).c_str());
+    return false;
+  }
+  
+  // HTTP header has been send and Server response header has been handled
+  Log_info("[HTTPS] POST OK, code: %d", httpCode);
+
+  https.end();
+
+  return true;
 }
