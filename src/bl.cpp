29e135b31f8f179cb11d9b7119171d0df864fec2
James Thurley <james@thurley.com>
2025-03-30 13:38:42 +0100

    Use constant for s to ms conversion

--- src/bl.cpp
+++ src/bl.cpp
@@ -1055,7 +1055,7 @@ static https_request_err_e downloadAndShow()
       if (requestedTimeout > 0) {
         // Convert from seconds to milliseconds.
         // A uint32_t should be large enough not to worry about overflow for any reasonable timeout.
-        requestedTimeout *= 1000;
+        requestedTimeout *= MS_TO_S_FACTOR;
         if (requestedTimeout > UINT16_MAX) {
           // To avoid surprising behaviour if the server returned a timeout of more than 65 seconds
           // we will send a log message back to the server and truncate the timeout to the maximum.
