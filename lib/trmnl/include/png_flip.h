5ba6ec74410d2053251883309a0cf0f1bfec785e
John Ruble <johnruble@gmail.com>
2025-04-11 11:39:47 -0400

    move png-flip to lib/trmnl and add tests
    reduce diff

---
+++ lib/trmnl/include/png_flip.h
@@ -0,0 +1,9 @@
+
+#include <stdlib.h>
+#include <string.h>
+
+unsigned char reverse_bits(unsigned char b);
+
+void flip_image(unsigned char *buffer, int width, int height);
+
+void horizontal_mirror(unsigned char *buffer, int width, int height);
