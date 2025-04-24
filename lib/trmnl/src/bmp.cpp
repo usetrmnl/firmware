6779257a0fba7150714a7a23c69c089456071e3d
John Ruble <johnruble@gmail.com>
2025-04-24 00:25:21 -0400

    use Log_ macros more widely
    everything but bl.cpp
    
    remove arduinolog includes

--- lib/trmnl/src/bmp.cpp
+++ lib/trmnl/src/bmp.cpp
@@ -13,7 +13,7 @@ bmp_err_e parseBMPHeader(uint8_t *data, bool &reversed)
   // Check if the file is a BMP image
   if (data[0] != 'B' || data[1] != 'M')
   {
-    Log.fatal("%s [%d]: It is not a BMP file\r\n", __FILE__, __LINE__);
+    Log_fatal("It is not a BMP file");
     return BMP_NOT_BMP;
   }
   // Get width and height from the header
@@ -30,7 +30,7 @@ bmp_err_e parseBMPHeader(uint8_t *data, bool &reversed)
   uint32_t dataOffset = *(uint32_t *)&data[10];
 
   // Display BMP information
-  Log.info("%s [%d]: BMP Header Information:\r\nWidth: %d\r\nHeight: %d\r\nBits per Pixel: %d\r\nCompression Method: %d\r\nImage Data Size: %d\r\nColor Table Entries: %d\r\nData offset: %d\r\n", __FILE__, __LINE__, width, height, bitsPerPixel, compressionMethod, imageDataSize, colorTableEntries, dataOffset);
+  Log_info("BMP Header Information:\r\nWidth: %d\r\nHeight: %d\r\nBits per Pixel: %d\r\nCompression Method: %d\r\nImage Data Size: %d\r\nColor Table Entries: %d\r\nData offset: %d", width, height, bitsPerPixel, compressionMethod, imageDataSize, colorTableEntries, dataOffset);
 
   // Check if there's a color table
   if (dataOffset > 54)
@@ -39,25 +39,25 @@ bmp_err_e parseBMPHeader(uint8_t *data, bool &reversed)
     uint32_t colorTableSize = colorTableEntries * 4; // Each color entry is 4 bytes
 
     // Display color table
-    Log.info("%s [%d]: Color table\r\n", __FILE__, __LINE__);
+    Log_info("Color table");
     for (uint32_t i = 0; i < colorTableSize; i += 4)
     {
-      Log.info("%s [%d]: Color %d: B-%d, R-%d, G-%d\r\n", __FILE__, __LINE__, i / 4 + 1, data[54 + 4 * i], data[55 + 4 * i], data[56 + 4 * i], data[57 + 4 * i]);
+      Log_info("Color %d: B-%d, R-%d, G-%d, A-%d", i / 4 + 1, data[54 + i], data[55 + i], data[56 + i], data[57 + i]);
     }
 
     if (data[54] == 0 && data[55] == 0 && data[56] == 0 && data[57] == 0 && data[58] == 255 && data[59] == 255 && data[60] == 255 && data[61] == 0)
     {
-      Log.info("%s [%d]: Color scheme standart\r\n", __FILE__, __LINE__);
+      Log_info("Color scheme standart");
       reversed = false;
     }
     else if (data[54] == 255 && data[55] == 255 && data[56] == 255 && data[57] == 0 && data[58] == 0 && data[59] == 0 && data[60] == 0 && data[61] == 0)
     {
-      Log.info("%s [%d]: Color scheme reversed\r\n", __FILE__, __LINE__);
+      Log_info("Color scheme reversed");
       reversed = true;
     }
     else
     {
-      Log.info("%s [%d]: Color scheme demaged\r\n", __FILE__, __LINE__);
+      Log_info("Color scheme demaged");
       return BMP_COLOR_SCHEME_FAILED;
     }
     return BMP_NO_ERR;
