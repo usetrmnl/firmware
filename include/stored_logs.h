5cb1479a6995fd80cf893f2de4d970cb7fd9d04d
John Ruble <johnruble@gmail.com>
2025-05-29 01:42:19 -0400

    move stored_logs into library and add tests

--- include/stored_logs.h
+++
@@ -1,7 +0,0 @@
-#include <Preferences.h>
-
-void store_log(const char *log_buffer, size_t size, Preferences &preferences);
-
-void gather_stored_logs(String &log, Preferences &preferences);
-
-void clear_stored_logs(Preferences &preferences);
